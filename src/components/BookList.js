e88f501d7393d32ed22c3999ecbc5da0f3a5cb04
sadakchap <aliceprerna@gmail.com>
2020-09-03 11:32:21 +0530

    created bookList & BookItem component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { BookContext } from '../contexts/BookContext';
+import BookItem from './BookItem';
 
 const BookList = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+
+    const { books } = useContext(BookContext);
+
+    return books.length ? (
+      <div className="book-list">
+        <ul>
+            { books.map(book => {
+                return ( <BookItem key={book.id} book={book} />)
+            }) }
+        </ul>
+      </div>
+    ) : (
+      <div className="empty">
+        No books to read. Hello free time <span role="img" aria-label="emoji">😄</span>
+      </div>
+    );
 }
 
 export default BookList
